31c7fec9a77547ab82cef41497f4c8f77621fbbf
cstenkamp <cstenkamp@uos.de>
2017-07-07 13:02:41 +0200

    Added Categories like problems&solution, added the DONEs about the sourcecodes

---
+++ pull_all.bat
@@ -0,0 +1,6 @@
+git pull origin master
+cd ../BA-rAIce-ANN
+git pull origin master
+cd ../BA-rAIce
+git pull origin master
+
---
+++ push_all.bat
@@ -0,0 +1,12 @@
+git add .
+git commit -m "autopush"
+git push origin master
+cd ../BA-rAIce-ANN
+git add .
+git commit -m "autopush"
+git push origin master
+cd ../BA-rAIce
+git add .
+git commit -m "autopush"
+git push origin master
+
